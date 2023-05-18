bc54ff15e5001963b1082b4c1c3af5668917cf60
SunnnySong <tjsghktjswls@naver.com>
2023-05-18 23:07:22 +0900

    start: project 생성

---
+++ ios-menu-app/ios-menu-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-menu-app
+//
+//  Created by Sunny on 2023/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
