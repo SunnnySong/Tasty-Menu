bf0fb19adbb587e54b57d03816c2511a595db23e
SunnnySong <tjsghktjswls@naver.com>
2023-05-18 23:46:25 +0900

    feat: TabBarController에 viewControllers 설정
    - TabBarModel 타입 생성
    - UINavigationController 를 생성하고, 해당 tabBarItem을 설정하는 함수 생성 및 설정

--- ios-menu-app/ios-menu-app/Controllers/TabBar/TabBarController.swift
+++ ios-menu-app/ios-menu-app/Controllers/TabBar/TabBarController.swift
@@ -12,5 +12,22 @@ final class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureViewControllers()
+        selectedIndex = 1
+    }
+    
+    private func configureViewControllers() {
+    
+        self.viewControllers = TabBarModel.allCases.map { tabBarViewController($0) }
+    }
+    
+    private func tabBarViewController(_ type: TabBarModel) -> UINavigationController {
+        
+        let viewController = UINavigationController(rootViewController: type.viewController)
+        viewController.tabBarItem.title = type.title
+        viewController.tabBarItem.image = type.image
+        
+        return viewController
     }
 }
+
