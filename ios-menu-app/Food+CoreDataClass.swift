212267bf033b73de594f23d4f115a43273721958
SunnnySong <tjsghktjswls@naver.com>
2023-05-25 00:27:32 +0900

    feat: CoreData Entity 생성
    - CoreData에 Food, Menu Entity 생성
    - AppDelegate에 Container에 접근하는 코드 추가

---
+++ ios-menu-app/Food+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Food+CoreDataClass.swift
+//  ios-menu-app
+//
+//  Created by Sunny on 2023/05/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Food)
+public class Food: NSManagedObject {
+
+}
---
+++ ios-menu-app/Menu+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Menu+CoreDataClass.swift
+//  ios-menu-app
+//
+//  Created by Sunny on 2023/05/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Menu)
+public class Menu: NSManagedObject {
+
+}
