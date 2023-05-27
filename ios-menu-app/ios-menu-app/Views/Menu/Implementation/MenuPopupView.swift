e39d057f970f35ceed79245be9d562bd9b2d2f2e
SunnnySong <tjsghktjswls@naver.com>
2023-05-28 01:01:41 +0900

    feat: MenuPopupView에서 ToolBar 추가

--- ios-menu-app/ios-menu-app/Views/Menu/Implementation/MenuPopupView.swift
+++ ios-menu-app/ios-menu-app/Views/Menu/Implementation/MenuPopupView.swift
@@ -10,17 +10,37 @@ import UIKit
 final class MenuPopupView: UIView {
     
     // MARK: Properties - View
-    private let popupStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.backgroundColor = .systemBackground
-        return stackView
+    private let popupView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBackground
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
-    private let headerDateView = MenuHeaderDateView(headerDate: .today)
+    private let headerDateView = HeaderDateView(headerDate: .today, type: .menu)
     
     private let menuCollectionView = MenuCollectionView(frame: .zero)
+    
+    private lazy var toolBarAddButton: ToolBarButton = {
+        let button = ToolBarButton(type: .add)
+        button.addTarget(self, action: #selector(didTaptoolBarAddButton), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var toolBarCloseButton: ToolBarButton = {
+        let button = ToolBarButton(type: ._close)
+        button.addTarget(self, action: #selector(didTaptoolBarCloseButton), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var toolBarStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.addArrangedSubviews([toolBarAddButton, toolBarCloseButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
 
     // MARK: Lifecycle
     override init(frame: CGRect) {
@@ -28,11 +48,16 @@ final class MenuPopupView: UIView {
         
         configureMenuPopupView()
         configureHierarchy()
-        configurePopupStackView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        configurePopupViewHierarchy()
     }
     
     // MARK: Functions - Private
@@ -40,24 +65,47 @@ final class MenuPopupView: UIView {
         
         backgroundColor = .designSystem(.mainBlack)?.withAlphaComponent(0.6)
         autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        addSubview(popupStackView)
     }
     
     private func configureHierarchy() {
         
-        let popupStackViewWidth = frame.width * 0.87
+        addSubview(popupView)
         
+        let popupStackViewWidth = frame.width * 0.87
         NSLayoutConstraint.activate([
-            popupStackView.widthAnchor.constraint(equalToConstant: popupStackViewWidth),
-            popupStackView.heightAnchor.constraint(equalToConstant: popupStackViewWidth),
-            popupStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            popupStackView.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+            popupView.widthAnchor.constraint(equalToConstant: popupStackViewWidth),
+            popupView.heightAnchor.constraint(equalToConstant: popupStackViewWidth),
+            popupView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            popupView.centerYAnchor.constraint(equalTo: self.centerYAnchor)
         ])
     }
     
-    private func configurePopupStackView() {
+    private func configurePopupViewHierarchy() {
         
-        popupStackView.addArrangedSubviews([headerDateView, menuCollectionView])
+        popupView.addSubview(headerDateView)
+        popupView.addSubview(toolBarStackView)
+        
+        let popupViewHeight = popupView.frame.height
+        let headerDateViewHeight = popupViewHeight * 0.275
+        let toolBarStackViewHeight = popupViewHeight * 0.15
+        NSLayoutConstraint.activate([
+            headerDateView.topAnchor.constraint(equalTo: popupView.topAnchor),
+            headerDateView.leadingAnchor.constraint(equalTo: popupView.leadingAnchor),
+            headerDateView.trailingAnchor.constraint(equalTo: popupView.trailingAnchor),
+            headerDateView.heightAnchor.constraint(equalToConstant: headerDateViewHeight),
+            
+            toolBarStackView.bottomAnchor.constraint(equalTo: popupView.bottomAnchor),
+            toolBarStackView.leadingAnchor.constraint(equalTo: popupView.leadingAnchor),
+            toolBarStackView.trailingAnchor.constraint(equalTo: popupView.trailingAnchor),
+            toolBarStackView.heightAnchor.constraint(equalToConstant: toolBarStackViewHeight)
+        ])
+    }
+    
+    @objc private func didTaptoolBarAddButton() {
+        print("didTaptoolBarAddButton")
+    }
+    
+    @objc private func didTaptoolBarCloseButton() {
+        print("didTaptoolBarCloseButton")
     }
 }
