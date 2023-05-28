cc0f94d7b2b2395ff9c1ce2f802f6fc0cec40f88
SunnnySong <tjsghktjswls@naver.com>
2023-05-28 21:11:53 +0900

    refactor: MenuCell autoLayout 수정 및 constant 변수 선언

--- ios-menu-app/ios-menu-app/Resources/DesignSystem/Images/SystemName.swift
+++ ios-menu-app/ios-menu-app/Resources/DesignSystem/Images/SystemName.swift
@@ -12,4 +12,5 @@ enum ImageSystemName: String {
     case chevronLeft = "chevron.left"
     case chevronRight = "chevron.right"
     case heartFill = "suit.heart.fill"
+    case photo = "photo"
 }
--- ios-menu-app/ios-menu-app/Views/Menu/Implementation/MenuCell.swift
+++ ios-menu-app/ios-menu-app/Views/Menu/Implementation/MenuCell.swift
@@ -14,8 +14,9 @@ final class MenuCell: CollectionViewCellProvidable {
     // MARK: Properties - View
     private var foodImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.backgroundColor = .systemPink
         imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .white
+        imageView.tintColor = .designSystem(.mainOrange)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
@@ -26,17 +27,15 @@ final class MenuCell: CollectionViewCellProvidable {
                                     color: .designSystem(.toolBarBlack),
                                     textAlignment: .left)
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = .green
         return label
     }()
     
     private var categoryLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .pretendard(size: 12, weight: .regular)
-        label.textAlignment = .center
+        let label = PretendardLabel(size: 12,
+                                    weight: .semibold,
+                                    color: .white,
+                                    textAlignment: .center)
         label.clipsToBounds = true
-        label.layer.cornerRadius = 32 / 2
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -50,7 +49,8 @@ final class MenuCell: CollectionViewCellProvidable {
     
     func configure(with item: DailyFood) {
         
-        foodImageView.image = item.image
+        let emptyImage = UIImage(systemName: ImageSystemName.photo.rawValue)
+        foodImageView.image = item.image == nil ? emptyImage : item.image
         foodLabel.text = item.name
         
         categoryLabel.text = item.category.title
@@ -63,22 +63,29 @@ final class MenuCell: CollectionViewCellProvidable {
         contentView.addSubview(foodLabel)
         contentView.addSubview(categoryLabel)
         
+        let cellHeight = self.frame.height
+        let cellWidth = self.frame.width
+        let itemSpacing: CGFloat = 12
+        let labelVerticalSapcing: CGFloat = 4
+        let labelWidth = cellWidth * 0.173
+        
+        categoryLabel.layer.cornerRadius = (cellHeight - labelVerticalSapcing * 2) / 2
+        
         NSLayoutConstraint.activate([
             foodImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
             foodImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             foodImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-            foodImageView.widthAnchor.constraint(equalToConstant: 40),
+            foodImageView.widthAnchor.constraint(equalToConstant: contentView.frame.height),
             
             foodLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
-            foodLabel.leadingAnchor.constraint(equalTo: foodImageView.trailingAnchor, constant: 12),
+            foodLabel.leadingAnchor.constraint(equalTo: foodImageView.trailingAnchor, constant: itemSpacing),
             foodLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
             
-            categoryLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4),
-            categoryLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -4),
-            categoryLabel.leadingAnchor.constraint(equalTo: foodLabel.trailingAnchor, constant: 12),
+            categoryLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: labelVerticalSapcing),
+            categoryLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -labelVerticalSapcing),
+            categoryLabel.leadingAnchor.constraint(equalTo: foodLabel.trailingAnchor, constant: itemSpacing),
             categoryLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            categoryLabel.widthAnchor.constraint(equalToConstant: 53),
-            categoryLabel.heightAnchor.constraint(equalToConstant: 32)
+            categoryLabel.widthAnchor.constraint(equalToConstant: labelWidth)
         ])
     }
 }
