5adf560ebc15f31f42337af48dce84d9a80a4706
SunnnySong <tjsghktjswls@naver.com>
2023-05-30 19:53:12 +0900

    feat: FoodModalView에서 사용할 FoodListStackView class 생성

---
+++ ios-menu-app/ios-menu-app/Resources/DesignSystem/UIObjects/FoodListTextField.swift
@@ -0,0 +1,51 @@
+//
+//  FoodListTextField.swift
+//  ios-menu-app
+//
+//  Created by Sunny on 2023/05/30.
+//
+
+import UIKit
+
+final class FoodListTextField: UIStackView {
+    
+    enum FoodListType: String {
+        case menu = "메뉴"
+        case category = "카테고리"
+    }
+
+    private var titleLabel: UILabel = {
+        let label = PretendardLabel(size: 17,
+                                    weight: .semibold,
+                                    color: .designSystem(.mainBlack),
+                                    textAlignment: .left)
+        return label
+    }()
+    
+    private var textField: UITextField = {
+        let textField = UITextField()
+        textField.font = .pretendard(size: 15, weight: .regular)
+        return textField
+    }()
+    
+    init(type: FoodListType) {
+        super.init(frame: .zero)
+        
+        spacing = 10
+        titleLabel.text = type.rawValue
+        configureHierarchy()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureHierarchy() {
+        
+        addArrangedSubviews([titleLabel, textField])
+
+        let titleLabelWidth: CGFloat = 60
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.widthAnchor.constraint(equalToConstant: titleLabelWidth).isActive = true
+    }
+}
---
+++ ios-menu-app/ios-menu-app/Views/Food/FoodModalView.swift
@@ -0,0 +1,20 @@
+//
+//  FoodModalView.swift
+//  ios-menu-app
+//
+//  Created by Sunny on 2023/05/30.
+//
+
+import UIKit
+
+class FoodModalView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
